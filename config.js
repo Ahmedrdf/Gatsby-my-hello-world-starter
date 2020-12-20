5adfba69cd2e86babed6a96a79f158cfd0852f6a
Ahmed Mansour <ahmedrdf.mansour@gmail.com>
2020-12-20 16:05:04 +0100

    first version

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Resume', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Ahmed',
+  lastName: 'Mansour',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/Ahmedrdf',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/ahmed-mansour-2a3939187/',
+    },
+ {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/AhmedMa42705636',
+    },
+   /* {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },*/
+  ],
+  email: 'ahmedrdf.mansour@gmail.com',
+  phone: '56236192',
+  address: 'wardia 4, Tunis, Tunisie ',
+};
